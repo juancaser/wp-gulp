65e68b78da903283e89f0c08a202b8b1dd76a4fc
Juan Caser <caserjan@gmail.com>
2019-04-30 00:14:01 +0800

    Initial commit for wp-gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,3 @@
-# WP + Gulp 4 task workflow
-## Requisites
-
-To install node and gulp please refer to ther respected documention instructions. Once you've done that you need to install this following node modules:
-
-```
-$ npm install gulp gulp-sass gulp-sourcemaps
-```
-
-## Customization
-
-To customise, please edit `gulpfile.js`and change the path according to your directory structure.
-
-```javascript
 const { src, dest, watch, series, parallel } = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
@@ -31,15 +17,7 @@ var tasks = {
         return src([tasks.path.scripts]).pipe(dest('./js'));
     },
     watch: function(){
-        watch(
-          [tasks.path.styles, tasks.path.scripts], 
-          parallel(tasks.styles, tasks.scripts)
-        );    
+        watch([tasks.path.styles, tasks.path.scripts], parallel(tasks.styles, tasks.scripts));    
     }
 };
-exports.default = series(
-  parallel(tasks.styles, tasks.scripts), 
-  tasks.watch
-);
-```
-
+exports.default = series(parallel(tasks.styles, tasks.scripts), tasks.watch);
